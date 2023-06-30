56ac9878f159f84d0b7ac5481a35763569c98f73
guy32807 <guy32807@comcast.net>
2023-06-30 05:03:43 -0400

    Initial commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,8 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def index(request):
+    return render(request, 'index.html', {})
 
 # Create an `about` view to render a static about page
 # def about(request):
